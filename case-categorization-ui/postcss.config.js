587d758789e6478ff8cf1faf71b6d2a429b39eed
Shaya Sh. <shaya.shakib@gmail.com>
2025-06-05 19:04:04 -0400

    Renamed frontend folder

--- case-categorization-ui/postcss.config.js
+++
@@ -1,7 +0,0 @@
-// postcss.config.js
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {}, // make sure you installed this package
-    autoprefixer: {},
-  },
-};
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+// frontend/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // This line is crucial for finding your React components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
